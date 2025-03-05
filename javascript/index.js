7f031efd5d0f614c11a33cafd1e1bd4e1b2288d6
10kc-session <165980049+10kc-session@users.noreply.github.com>
2025-03-05 15:39:16 +0530

    Arrays in js

--- javascript/index.js
+++ javascript/index.js
@@ -1,168 +1,160 @@
 /**
- *  Loops in Javascript
- *  -------------------
- *      Loops are used to execute block of code multiple times untill the condition 
- *      satisfies
- *      types of loops
- *      -------------
- *      for loop , while loop , do while , for in , for off
- */
-
-// unary operator in js
-/**
- *  ++ -> increment  -> post increment , pre increment -> +1 
- *  -- -> decrement  -> post decrement , pre decrement -> -1
- */
-var a = 10; // 11
-// a++;  // post increment a = a + 1;
-console.log(a++); // first accessing the value and later increment value  -> 10
-// a = a + 1; -> 11
-console.log(a);
-console.log("--------------------");
-var b = 20; // 21
-// ++b; // pre increment first update the value b = b + 1 -> b = 20 + 1 -> 21 later access
-console.log(++b);
-console.log(b);
-
-var c = 10; // 11
-//           10 + 11
-console.log(c++ + c); // 21
-console.log(c); // 11
-
-console.log("-----------");
-console.clear();
-var d = 35; // 37
-//           35 + 36
-console.log(d++ + d++); // 71
-console.log(d); // 37
-
-d = 4;
-console.log(d++ + ++d);
-console.log(++d + d++ - d);
-
-/**
- *   for loop
- *  -----------
- *      Syn : 
- *              for identifier in range(start , end , step):
- *                  // statements
+ *  Arrays in Javascript
+ *  --------------------
+ *      Arrays are non primitive datatypes or special type of object which can 
+ *      heterogenous and homogenous values in a single variable 
+ *      In javascript , for arrays there is no restriction on length
+ *      The Values which we store arrays are known as elements
+ *      To find lenght of array , we can use length property
+ *      In Javascript array index starts from 0
+ *      In Array elements are seperated by , and array is represented in []
+ *      Arrays are in form of continous memory allocation
+ *      There are two ways to create objects 
+ *      -------------------------------------
+ *          1. using literal (Recommended Way)
+ *              Syn : 
+ *                      varaiable refVar = [];
+ *          2. using constructor 
+ *              Syn : 
+ *                      variable refVar = new Array();
+ *      Curd operations
+ *      ----------------
+ *       create ,               update ,                read  ,                 delete
+ *       inserting the elements    modifying the elements     Accessing the elements      delete the elements
  * 
- *              for(initilization;condition;updation){
- *                      // statements
- *              }
- *              initilization -> baby born 
- *              condition     -> is alive or not 
- *              updation      -> birthday 
- 
- * First Loop -> initilization and condition
- * Second loop to end -> updation and condition
+ *      How to insert into an element into array
+ *     -------------------------------------------
+ *              varaible refVar = [];
+ *              refVar[index] = value; // inserting the value 
+ *              varaible refVar = [element1 , element2,.....elementN]; // inserting values
+ *      
+ *      varaiable identifier = refVar[index] // accessing the value
+ *      
+ *      refVar[alreadyAvailableIndex] = value; // updating the value
+ * 
+ *      We cannot Delete Elements from array manually , but with the help of methods
+ *      we can delete the elements
+ *  
+ *          refVar.pop(); // deletes the last element
+ *          refVar.shift(); // deleted the first element
+ *          refVar.splice(); // deletes elements from one position another position
  */
-for (var a = 1; a <= 5; a = a + 1) {
-    console.log(a);
-}
 
-for (var a = 1; a <= 100; a++) {
-    console.log(a);
-}
+var arr = [];
+console.log(typeof arr); // object
 
-//  i = i + 2
-for (var i = 1; i <= 14; i += 2) {
-    console.log(i);
-}
+//  Inserting the values
+arr[0] = 10;
+arr[1] = 30;
+arr[2] = "Javascript";
+arr[3] = function () { console.log("Hello World") };
 
-for (var i = 0; i % 2 == 0; i++) {
-    console.log(i);
-}
+console.log(arr.length); // 4
+console.log(arr); // [10 , 30 , "Javascript" , function]
 
-for (var i = 0; i % 2; i++) {
-    console.log(i);
-}
+// Accessing the values
 
-for (var i = 1; i < 6; i++) {
-    console.log(i % 3); // 1 2 0 1 2
-}
+console.log(arr[3]);
+arr[3]();
+
+console.log(arr[0] + arr[1]);
+arr[5] = arr[0] * 2; // inserting value into 5th index
+
+console.log(arr[4]);
+console.log(arr.length); // 6
+
+// how to access last element from array
+// given by suraj
+
+console.log(arr[arr.length - 1]); // last index
+
+
+console.clear();
+
+//     0   1    2    3      4      5        6
+
+console.log(arr.length);
+console.log(arr);
+arr[0] = 45;
+console.log(arr);
+
 /**
- * Write a function which takes a single parameter from 1 to n , and prints the table
- * of the parameter value upto 10
- *  function name -> printTable
- *  parameter -> num 
- *  input -> 2
- *    output 2 table -> 2 * 1 = 2
- *                      2 * 2 = 4
- */
-
-function printTable(num) {
-    for (var i = 1; i <= 10; i++) {
-        console.log(`${num} * ${i} = ${num * i}`);
-    }
-}
+ * for(init ; condition ; updation){
+ *      // statements
+ *  }
+*/
 console.clear();
-printTable(2);
 
-//-----------------------------------------------
+arr = [1, 'a', 'b', false, 0.0, 1.23455, function () { return "Hey There" }];
+
+for (var index = 0; index <= 6; index++) {
+    console.log(arr[index]);
+}
+
+console.log("-------------------------------");
+
+for (var index = 0; index <= arr.length - 1; index++) {
+    console.log(arr[index]);
+}
+
+console.clear();
+
+arr = [1, 2, 3, 4, 5, 6];
+
+// returns elements
+for (var element of arr) {
+    console.log(element);
+}
+
+// returns indexes
+for (var index in arr) {
+    console.log(arr[index])
+}
+
+arr = [32, 4, 56, 732, 45, 23, 18, 81, "seventy seven"];
+
+for (var i = arr.length - 1; i >= 0; i--) {
+    console.log(arr[i]);
+}
+
+console.clear();
+
+arr = [5, 4, 3, 6, 7, 8, 9];
+console.log(arr);
+arr.pop();
+console.log(arr);
+arr.shift();
+console.log(arr); //[4, 3, 6, 7, 8]
+arr.splice(1, 3);
+console.log(arr);
+
+
 
 /**
- * prime -> 1 to n
- *  -> it should divisible by one
- *  -> and itself
- */
-console.clear();
-var num = 3;
-console.log(num % 1);
-console.log(num % 3);
+ *      find even numbers from given array 
+ *          o/p => even numbers
+ *          arr = [6,4,7,8,9,32,3,4,5,5,43,2,12,3,4,5,67,8,987,65,43,2,234,56]
+*/
+arr = [6, 4, 7, 8, 9, 32, 3, 4, 5, 5, 43, 2, 12, 3, 4, 5, 67, 8, 987, 65, 43, 2, 234, 56]
 
-num = 6;
-console.log(num % 1);
-console.log(num % 6);
-console.log(num % 2);
-console.log(num % 3);
-
-num = 100;
-
-num = 7;
-console.log(num % 1);
-console.log(num % 7);
-
-// 5
-// 1 itself -> 2
-// 1 -> x  ifself -> X
-//  1 2 3 4 5 6 7 -> 3.5 -> 3
-//  1 2 3 4 5 6
-//   9
-//  1 2 3 4 5 6 7 8 9 10 / 20
-console.clear();
-//               100
-//  2 to 50 
-function isPrime(num) {
-    if (num == 1)
-        return false;
-    //   i = 2 ; i <= 50
-    for (var i = 2; i <= Math.floor(num / 2); i++) {
-        if (num % i == 0) {
-            return false;
-        }
-    }
-    return true;
-}
-if (isPrime(100)) {
-    console.log("Is Prime Number");
-} else {
-    console.log("! Prime");
-}
-
-var num = 100;
-var count = 0; // 3
-// 1 to 100
-for (var i = 1; i <= num; i++) {
-    if (num % i == 0) {
-        count++;
-    }
-    if (count > 2) {
-        break;
+for (var element of arr) {
+    if (element % 2 == 0) {
+        console.log(element);
     }
 }
-if (count == 2) {
-    console.log("Prime Number");
-} else {
-    console.log("!");
+
+for (var i = 0; i <= arr.length - 1; i++) {
+    if (arr[i] % 2 == 0) {
+        console.log(arr[i]);
+    }
 }
+
+console.log("-------------------");
+
+var i = 0;
+while (i < arr.length) {
+    if (arr[i] % 2 == 0)
+        console.log(arr[i]);
+    i++;
+}
+
