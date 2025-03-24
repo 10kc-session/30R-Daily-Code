97225a87f03f4f4f6b3ba1b84b8a158bca3307ce
10kc-session <165980049+10kc-session@users.noreply.github.com>
2025-03-24 16:15:45 +0530

    BOM and API

--- javascript/index.js
+++ javascript/index.js
@@ -1,73 +1,66 @@
-/**
- *    BOM (browser object modal)
- *    ---------------------------
- *      -> Bom is used to perform manipulations browser , it can access location , history.
- *      we can create a new window using bom.
- *      -> Bom is represented in form of window object
- *          location object 
- *          navigation object
- *          screen object 
- *          document object 
- *      -> storages ->
- *              localStorage   -> it will store the data inside browser even after closing the tab or browser
- *                                  maximum 5mb to 10mb -> lifetime
- *              sessionStorage -> it will store data for particular tab , 5mb -> data storage
- *              cookie         -> lifetime , store the data -> 4kb
- * 
- *  we have to filter the data 
- *      fakestore api 
- *      
- */
+let container = document.getElementsByClassName("container")[0];
+let btnContainer = document.getElementsByClassName("btn-container")[0];
 
-// localStorage.setItem("name", "rakesh");
-// localStorage.setItem("address", "hyderebad");
-
-// let name = localStorage.getItem("name");
-// console.log(name);
-
-// let address = localStorage.getItem("address");
-// console.log(address);
-
-// let arr = [1, 2, 3, 4, 5, 6]; // string -> converts non to string
-
-// localStorage.setItem("values", JSON.stringify(arr));
-
-// let res = JSON.parse(localStorage.getItem("values")); // string to non primitive
-
-// console.log(res);
-
-let form = document.querySelector("form");
-let input = document.getElementById("name");
-
-form.addEventListener("submit", function (event) {
-    if (input.value == '') {
-        event.preventDefault();
-    }
-    else {
-        //                    null || [] , ["rakesh"] || []
-        let arr = JSON.parse(localStorage.getItem("names")) || []; // ["Rakesh" , "Hemanth"]
-        arr.push(input.value); // ["Rakesh" , "Hemanth" , "Suraj"]
-        localStorage.setItem("names", JSON.stringify(arr));
-        displayData();
-    }
-})
-
-function displayData() {
-    let container = document.querySelector(".container");
-    container.innerHTML = ``;
-    let data = JSON.parse(localStorage.getItem("names"));
-    if (data == null) {
-        container.innerHTML = `No Data Available`;
-    } else {
-        data.forEach(ele => {
-            let p = document.createElement("p");
-            p.innerHTML = `Name : ${ele}`;
-            container.appendChild(p);
-        })
+async function getData() {
+    try {
+        let response = await fetch("https://fakestoreapi.com/products");
+        if (!response.ok) {
+            throw new Error("HTTP Errror : ", response.status);
+        }
+        let result = await response.json();
+        localStorage.setItem("products", JSON.stringify(result));
+        let products = JSON.parse(localStorage.getItem("products"));
+        displayData(products);
+    } catch (err) {
+        console.error(err);
     }
 }
 
+function displayData(products) {
+    container.innerHTML = ``;
+    if (products == null) {
+        container.innerHTML = `<h1>No Data Available</h1>`;
+    }
+    else {
+        products.forEach(obj => {
+            let { image, title, price, description, category } = obj;
+            let item = document.createElement("div");
+            item.id = "item";
+            item.innerHTML = `
+            <img src = '${image}'> 
+            <p>Title : ${title} , Price : ${price}</p>
+            <p>Description : ${description}</p>
+            <p><b>Category : ${category}</b></p>
+            `
+            container.appendChild(item);
+        });
+        displayButtons();
+    }
+}
+function displayButtons() {
+    btnContainer.innerHTML = ``;
+    let products = JSON.parse(localStorage.getItem("products"));
+    let allButton = document.createElement("button");
+    allButton.innerHTML = `All Data`;
+    allButton.addEventListener("click", function () {
+        displayData(products);
 
-window.addEventListener("DOMContentLoaded", function () {
-    displayData();
-})
+    })
+    btnContainer.appendChild(allButton);
+    let categoryArr = products.map(obj => obj.category);
+    Array.from(new Set(categoryArr)).forEach(ele => {
+        let button = document.createElement("button");
+        button.innerHTML = ele;
+        button.addEventListener("click", function () {
+            filterData(ele, products);
+        })
+        btnContainer.appendChild(button);
+    })
+}
+
+function filterData(ele, products) {
+    let categoryArr = products.filter(obj => obj.category == ele);
+    displayData(categoryArr);
+}
+
+getData();
